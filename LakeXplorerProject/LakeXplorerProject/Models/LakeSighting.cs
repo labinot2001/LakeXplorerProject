d80c2ef1426cfeb1d20949aca1c018730aa7a951
Labinot <krasniqilabinot42@gmail.com>
2023-08-03 20:19:57 +0200

    feature: created Lake, LakeSighting, and Like model

---
+++ LakeXplorerProject/LakeXplorerProject/Models/LakeSighting.cs
@@ -0,0 +1,18 @@
+﻿namespace LakeXplorerProject.Models
+{
+    public class LakeSighting
+    {
+        public int Id { get; set; } // Primary key
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string? Image { get; set; } // Store the image URL or local path
+
+       // public int UserId { get; set; }   //Reference to the user
+       // public virtual User User { get; set; }  
+
+       // public int LakeId { get; set; } // Reference to the lake
+       // public virtual Lake Lake { get; set; } 
+
+
+    }
+}
---
+++ LakeXplorerProject/LakeXplorerProject/Models/Like.cs
@@ -0,0 +1,15 @@
+﻿namespace LakeXplorerProject.Models
+{
+    public class Like
+    {
+        public int Id { get; set; } 
+       
+       // public int UserId { get; set; } // Reference to the user
+       // public virtual User User { get; set; } // Navigation property to access the User
+
+      //  public int LakeSightingId { get; set; } // Reference to the lake sighting
+       // public virtual LakeSighting LakeSighting { get; set; } // Navigation property to access the LakeSighting
+
+
+    }
+}
