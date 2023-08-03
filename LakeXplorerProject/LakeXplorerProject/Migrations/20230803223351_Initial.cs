572bfb5080a31b4f4562849a0c6ddc0c88cc8e02
Labinot <krasniqilabinot42@gmail.com>
2023-08-04 00:43:31 +0200

    feature: created initial migration

---
+++ LakeXplorerProject/LakeXplorerProject/Migrations/20230803223351_Initial.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LakeXplorerProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lakes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lakes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LakeSightings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Longitude = table.Column<double>(type: "float", nullable: false),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LakeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LakeSightings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LakeSightings_Lakes_LakeId",
+                        column: x => x.LakeId,
+                        principalTable: "Lakes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LakeSightingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Likes_LakeSightings_LakeSightingId",
+                        column: x => x.LakeSightingId,
+                        principalTable: "LakeSightings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LakeSightings_LakeId",
+                table: "LakeSightings",
+                column: "LakeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_LakeSightingId",
+                table: "Likes",
+                column: "LakeSightingId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Likes");
+
+            migrationBuilder.DropTable(
+                name: "LakeSightings");
+
+            migrationBuilder.DropTable(
+                name: "Lakes");
+        }
+    }
+}
