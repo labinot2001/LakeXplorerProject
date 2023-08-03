27dbd06facf11cedd3bb97307bae3ffb83996f05
Labinot <krasniqilabinot42@gmail.com>
2023-08-03 13:26:20 +0200

    Initial LakeXplorerProject

---
+++ LakeXplorerProject/LakeXplorerProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LakeXplorerProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
