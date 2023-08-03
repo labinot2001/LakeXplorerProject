a5a8b5978cfe11e6817334f6ad73eba82b90a706
Labinot <krasniqilabinot42@gmail.com>
2023-08-03 21:19:03 +0200

    feature: created connectionString, configuration connection string

--- LakeXplorerProject/LakeXplorerProject/Program.cs
+++ LakeXplorerProject/LakeXplorerProject/Program.cs
@@ -1,4 +1,5 @@
 using LakeXplorerProject.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //DbContext configuration
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString
+    ("DefaultConnectionString")));
 
 
 
