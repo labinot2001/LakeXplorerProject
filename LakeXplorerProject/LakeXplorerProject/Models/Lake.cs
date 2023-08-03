b27f9e17d3decd498f6f0de25b78d5ff43746339
Labinot <krasniqilabinot42@gmail.com>
2023-08-03 20:19:57 +0200

    feature: created Lake, LakeSighting, and Like model

---
+++ LakeXplorerProject/LakeXplorerProject/Models/Lake.cs
@@ -0,0 +1,11 @@
+﻿namespace LakeXplorerProject.Models
+{
+    public class Lake
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Image { get; set; } // Store the image URL or local path
+        public string? Description { get; set; }
+
+    }
+}
