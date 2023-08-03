3c3b1f08083da12a5608b21d18389f6e8cb99eb4
Labinot <krasniqilabinot42@gmail.com>
2023-08-04 01:40:06 +0200

    feature: created homepage template

--- LakeXplorerProject/LakeXplorerProject/Program.cs
+++ LakeXplorerProject/LakeXplorerProject/Program.cs
@@ -3,13 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 //DbContext configuration
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString
     ("DefaultConnectionString")));
-
 
 
 
