7ebae2bf727496683a3af99621039163328cd5b8
Labinot <krasniqilabinot42@gmail.com>
2023-08-04 18:32:46 +0200

    feature: created ILakeService, created LakeService, created LakeController and view

---
+++ LakeXplorerProject/LakeXplorerProject/Data/Services/ILakeServices.cs
@@ -0,0 +1,18 @@
+﻿using LakeXplorerProject.Data.Base;
+using LakeXplorerProject.Models;
+
+namespace LakeXplorerProject.Data.Services
+{
+    public interface ILakeServices : IEntityBaseRepository<Lake>
+    {
+        Task<Lake> GetCarByIdAsync(int id);
+        Task AddNewLakeAsync(Lake data);
+        Task UpdateLakeAsync(Lake data);
+        Task DeleteAsync(int id);
+
+        //Task AddNewLakeAsync(NewLakeVM data);
+        //Task UpdateLakeAsync(NewLakeVM data);
+        //Task<NewLakeDropdownsVM> GetNewLakeDropdownsValues();
+
+    }
+}
---
+++ LakeXplorerProject/LakeXplorerProject/Data/Services/LakeService.cs
@@ -0,0 +1,51 @@
+﻿using LakeXplorerProject.Data.Base;
+using LakeXplorerProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LakeXplorerProject.Data.Services
+{
+    public class LakeService : EntityBaseRepository<Lake>, ILakeServices
+    {
+        private readonly AppDbContext _context;
+        public LakeService(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task AddNewLakeAsync(Lake data)
+        {
+            var newLake = new Lake()
+            {
+                Name = data.Name,
+                Image = data.Image,
+                Description = data.Description,
+
+            };
+            await _context.Lakes.AddAsync(newLake);
+            await _context.SaveChangesAsync();
+        }
+
+
+
+        public Task<Lake> GetCarByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateLakeAsync(Lake data)
+        {
+            var dbLake = await _context.Lakes.FirstOrDefaultAsync(n => n.Id == data.Id);
+
+            if (dbLake != null)
+            {
+                dbLake.Name = data.Name;
+                dbLake.Image = data.Image;
+                dbLake.Description = data.Description;
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+    }
+}
